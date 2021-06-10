0541af1fcfab7f6c2a6cd8200d667beed79efa71
Henrik Olsen <henrik.olsen@telia.no>
2021-06-10 11:27:49 +0200

    added a baseline package.xml which have all the types and more. Started to build out the merger.py

--- merger.py
+++ merger.py
@@ -1,1 +1,27 @@
 #Merger file.
+
+import xml.etree.ElementTree as ET
+import os
+
+directory = './manifests'
+typesMap = {}
+
+def getManifests():
+    for filename in os.listdir(directory):
+        if filename != 'package.xml' and filename.endswith('.xml'):
+            print(filename)
+
+def buildConsolidateManifest():
+    tree = ET.parse('./manifests/package.xml')
+    root = tree.getroot()
+    for child in root.findall('types'):
+        typeName = child.find('name')
+        typeMembers = []
+        for member in child.iterfind('members'):
+            typeMembers.append(str(member.text))
+
+        typesMap[typeName] = typeMembers
+
+    print(typesMap)
+
+getManifests()
