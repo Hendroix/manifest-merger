261e501754e0f873cd7b226d388598ed2629c1e3
Henrik Olsen <henrik.olsen@telia.no>
2021-06-10 15:30:38 +0200

    Current changes stuck on indentation

--- merger.py
+++ merger.py
@@ -1,6 +1,7 @@
 #Merger file.
 
 import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import ElementTree
 import os
 
 directory = './manifests'
@@ -9,16 +10,38 @@ typesMap = {}
 def getManifests():
     for filename in os.listdir(directory):
         if filename != 'package.xml' and filename.endswith('.xml'):
-            print(filename)
             buildConsolitdatedManifest(directory + '/' + filename)
 
 def buildConsolitdatedManifest(filePath):
-    tree = ET.parse(filePath)
+    tree = ET.parse(r'%s' % filePath)
     root = tree.getroot()
     for child in root.findall('types'):
         typeName = child.find('name').text
-        for member in child.iterfind('members'):
-            typesMap[typeName][member.text]
+        if typeName not in typesMap:
+            typesMap[typeName] = set()
 
-getManifests()
-print(typesMap)
+        for member in child.iterfind('members'):
+            typesMap[typeName].add(member.text)
+
+def generateOutput(map):
+    package = ET.Element('package')
+    package.set('xmlns', 'http://soap.sforce.com/2006/04/metadata')
+    for key in map:
+        types = ET.SubElement(package, 'types')
+        name = ET.SubElement(types, 'name')
+        ET.indent(types, space='    ', level=1)
+        name.text = key
+        for item in map[key]:
+            members = ET.SubElement(types, 'members')
+            members.text = item
+
+    tree = ElementTree(package)
+    tree.write('./output/output.xml', encoding='utf-8', xml_declaration=True)
+        
+def main():
+    getManifests()
+    #print(typesMap)
+    generateOutput(typesMap)
+    
+if __name__ == "__main__":
+    main()
